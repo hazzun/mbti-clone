33407db0fa58f373cef5d216efab02e2e895d8e3
hazzun <gkwns0516@gmail.com>
2022-08-30 21:38:25 +0900

    prove result algorithm

--- js/index.js
+++ js/index.js
@@ -6,7 +6,52 @@ const FADEIN = "fadeInClick";
 const FADEOUT = "fadeOutClick";
 const HIDDEN_CLASS = "hidden";
 
-function addAnswer(lalala, qIdx) {
+const select = [];
+
+function calResult() {
+    //결과를 계산할 배열 생성
+    const pointArray = [
+        { name: 'mouse', value: 0, key: 0 },
+        { name: 'cow', value: 0, key: 1 },
+        { name: 'tiger', value: 0, key: 2 },
+        { name: 'rabbit', value: 0, key: 3 },
+        { name: 'dragon', value: 0, key: 4 },
+        { name: 'snake', value: 0, key: 5 },
+        { name: 'horse', value: 0, key: 6 },
+        { name: 'sheep', value: 0, key: 7 },
+        { name: 'monkey', value: 0, key: 8 },
+        { name: 'chick', value: 0, key: 9 },
+        { name: 'dog', value: 0, key: 10 },
+        { name: 'pig', value: 0, key: 11 },
+    ]
+    for (let i = 0; i < 12; i++) {
+        const target = qnaList[i].a[select[i]];
+        for (let j = 0; j < target.type.length; j++) {
+            for (let k = 0; k < pointArray.length; k++) {
+                if (target.type[j] === pointArray[k].name) {
+                    pointArray[k].value++;
+                }
+            }
+        }
+    }
+
+    const resultArray = pointArray.sort(function (a, b) {
+        if (a.value > b.value) {
+            return -1;
+        }
+        if (a.value < b.value) {
+            return 1;
+        }
+        return 0;
+    });
+    let resultKey = resultArray[0].key;
+
+    console.log(resultArray);
+    console.log(resultKey);
+    return resultKey;
+}
+
+function addAnswer(lalala, qIdx, aIdx) {
     const answerbox = document.querySelector(".answerBox");
     const answerButton = document.createElement("button");
     answerbox.appendChild(answerButton);
@@ -20,15 +65,15 @@ function addAnswer(lalala, qIdx) {
             const answerBtnAll = document.querySelectorAll(".answerBox button");
             // console.log(answerBtnAll);
             for (let i = 0; i < answerBtnAll.length; i++) {
-                console.log(answerBtnAll[i]);
+                // console.log(answerBtnAll[i]);
                 answerBtnAll[i].disabled = true;
                 answerBtnAll[i].style.display = 'none';
             }
-            // answerButton.classList.remove(FADEIN);
-            // answerButton.classList.add(FADEOUT);
-            // console.log(qIdx);
+            select[qIdx] = aIdx; //빈 배열의 몇번째 질문에 몇번째 답변이 담기는지 정보 담기
             qnaShow(++qIdx);
         } else {
+            select[qIdx] = aIdx;
+            console.log(select);
             qna.classList.add(FADEOUT);
             setTimeout(() => {
                 result.classList.add(FADEIN);
@@ -37,6 +82,8 @@ function addAnswer(lalala, qIdx) {
                     result.classList.toggle(HIDDEN_CLASS);
                 }, 0)
             }, 0)
+
+            calResult();
         }
     });
 }
@@ -46,11 +93,11 @@ function qnaShow(qIdx) {
     qbox.innerHTML = qnaList[qIdx].q;
 
     for (let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
 
     const progress = document.querySelector(".progress-bar");
-    console.log(qIdx);
+    // console.log(qIdx);
     // progress.style.width = `${(qIdx + 1) * 8}%`;
     progress.style.width = `${(100 / 12) * (qIdx + 1)}%`;
     progress.innerHTML = `${Math.floor((100 / 12) * (qIdx + 1))}%`;
